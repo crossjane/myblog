a62ecb0da05d12910400a626e9cc76c0aa03f719
crossjane <cross.jane.kim@gmail.com>
2025-05-15 19:24:16 +0900

    FIX

--- vite.config.js
+++ vite.config.js
@@ -6,4 +6,7 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   define: { global: "window" },
+  server: {
+    port: 3000,
+  },
 });
